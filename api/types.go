5405aea8336db89a20b56edc794e80d37db14fb9
Paul Tyng <ptyng@underarmour.com>
2016-11-21 18:22:55 -0500

    Flip configuration to map[string]interface{}

--- api/types.go
+++ api/types.go
@@ -70,9 +70,9 @@ type AlertChannelLinks struct {
 
 // AlertChannel represents a New Relic alert notification channel
 type AlertChannel struct {
-	ID            int               `json:"id,omitempty"`
-	Name          string            `json:"name,omitempty"`
-	Type          string            `json:"type,omitempty"`
-	Configuration map[string]string `json:"configuration,omitempty"`
-	Links         AlertChannelLinks `json:"links,omitempty"`
+	ID            int                    `json:"id,omitempty"`
+	Name          string                 `json:"name,omitempty"`
+	Type          string                 `json:"type,omitempty"`
+	Configuration map[string]interface{} `json:"configuration,omitempty"`
+	Links         AlertChannelLinks      `json:"links,omitempty"`
 }
