64469507d04d90c591ec1a20a1613569019dfe70
Paul Tyng <ptyng@underarmour.com>
2016-11-23 18:35:27 -0500

    Add applications

--- api/types.go
+++ api/types.go
@@ -71,3 +71,48 @@ type AlertChannel struct {
 	Configuration map[string]interface{} `json:"configuration,omitempty"`
 	Links         AlertChannelLinks      `json:"links,omitempty"`
 }
+
+type ApplicationSummary struct {
+	ResponseTime            float64 `json:"response_time"`
+	Throughput              float64 `json:"throughput"`
+	ErrorRate               float64 `json:"error_rate"`
+	ApdexTarget             float64 `json:"apdex_target"`
+	ApdexScore              float64 `json:"apdex_score"`
+	HostCount               int     `json:"host_count"`
+	InstanceCount           int     `json:"instance_count"`
+	ConcurrentInstanceCount int     `json:"concurrent_instance_count"`
+}
+
+type ApplicationEndUserSummary struct {
+	ResponseTime float64 `json:"response_time"`
+	Throughput   float64 `json:"throughput"`
+	ApdexTarget  float64 `json:"apdex_target"`
+	ApdexScore   float64 `json:"apdex_score"`
+}
+
+type ApplicationSettings struct {
+	AppApdexThreshold        float64 `json:"app_apdex_threshold,omitempty"`
+	EndUserApdexThreshold    float64 `json:"end_user_apdex_threshold,omitempty"`
+	EnableRealUserMonitoring bool    `json:"enable_real_user_monitoring,omitempty"`
+	UseServerSideConfig      bool    `json:"use_server_side_config,omitempty"`
+}
+
+type ApplicationLinks struct {
+	Servers              []int `json:"servers,omitempty"`
+	ApplicationHosts     []int `json:"application_hosts,omitempty"`
+	ApplicationInstances []int `json:"application_instances,omitempty"`
+	AlertPolicyID        int   `json:"alert_policy"`
+}
+
+type Application struct {
+	ID             int                       `json:"id,omitempty"`
+	Name           string                    `json:"name,omitempty"`
+	Language       string                    `json:"language,omitempty"`
+	HealthStatus   string                    `json:"health_status,omitempty"`
+	Reporting      bool                      `json:"reporting,omitempty"`
+	LastReportedAt string                    `json:"last_reported_at,omitempty"`
+	Summary        ApplicationSummary        `json:"application_summary,omitempty"`
+	EndUserSummary ApplicationEndUserSummary `json:"end_user_summary,omitempty"`
+	Settings       ApplicationSettings       `json:"settings,omitempty"`
+	Links          ApplicationLinks          `json:"links,omitempty"`
+}
